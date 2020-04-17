6f78f3da80647f14641601958a5550cebf38143c
sametweb <sametmutevellioglu@gmail.com>
2020-04-17 13:17:39 -0700

    stretch

--- data/models/project-model.js
+++ data/models/project-model.js
@@ -11,7 +11,26 @@ function find() {
 }
 
 function findById(id) {
-  return db("project").where({ id }).first();
+  return db("project")
+    .where({ id })
+    .first()
+    .then(async (project) => {
+      const project_id = project.id;
+
+      // find tasks for project_id
+      const tasks = await db("task")
+        .where({ project_id })
+        .select("id", "description", "notes", "completed");
+
+      //find resources for project_id
+      const resources = await db("project_resource")
+        .where({ project_id })
+        .join("resource", "project_resource.resource_id", "resource.id")
+        .join("project", "project.id", "project_resource.project_id")
+        .select("resource.id", "resource.name", "resource.description");
+
+      return { ...project, tasks, resources };
+    });
 }
 
 function add(projectData) {
